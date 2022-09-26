ac1ef3df8c5e8c07ef0e7b96116fb68aa31b355f
Minh Duy <minhduy140799@gmail.com>
2022-09-26 15:12:07 +0700

    almost done

--- src/App.js
+++ src/App.js
@@ -5,6 +5,28 @@ import { Skills, Banner, Navbar, BannerFooter, BannerMiddle } from './components
 
 import './App.css';
 
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyDMYaEgLgZs1Bn7Ve__Pq0D5zQk35fcJTg",
+  authDomain: "portfolio-70bdb.firebaseapp.com",
+  projectId: "portfolio-70bdb",
+  storageBucket: "portfolio-70bdb.appspot.com",
+  messagingSenderId: "747634434847",
+  appId: "1:747634434847:web:dc7faa73dcd9022cc8462a",
+  measurementId: "G-J4VFR1492Q"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
+
 const App = () => (
   <div className="app">
     <div className="gradient__bg">
